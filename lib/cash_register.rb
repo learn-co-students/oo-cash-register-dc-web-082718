c87c9a80973e63253d1e7b4f7fc18a93b99825eb
Trevor Jameson <github email address>
2018-09-04 13:29:05 -0400

    lesson in progress. apply discount test failing

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,33 @@
+require 'pry'
 
+class CashRegister
+
+  attr_accessor :total, :discount, :prev_total
+  attr_reader :items
+
+
+  def initialize(emp_discount = 0)
+    @total = 0
+    @discount = emp_discount
+    @items = []
+    @prev_total = @total
+  end
+  #total = 100
+  def add_item(title, price, op_quantity = 1)
+    op_quantity.times {@items << title}
+    @prev_total = price * op_quantity
+    self.total += @prev_total # Does += work here? I believe it failed when tested in irb yesterday.
+  end
+
+  def apply_discount
+    if @discount == 0
+      return "There is no discount to apply."
+    end
+    self.total = total * ((100.0 - discount.to_f)/100).to_i
+    return "After the discount, the total comes to $#{@total}."
+  end
+
+  def void_last_transaction
+    @total -= @prev_total
+  end
+end
